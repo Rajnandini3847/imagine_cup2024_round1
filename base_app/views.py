992c39ac3c4df1120df96ec4d3713baa587f54c3
gitone912 <akash.101@kvsroranchi.in>
2024-02-07 22:49:59 +0530

    first commit

---
+++ base_app/views.py
@@ -0,0 +1,52 @@
+from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+from django.contrib.auth  import authenticate,  login, logout
+
+# Create your views here.
+
+def landing_page(request):
+    return render(request, 'landing_page.html')
+
+def Register(request):
+    if request.method != "POST":
+        return render(request, "register.html")
+    username = request.POST['username']
+    email = request.POST['email']
+    first_name=request.POST['first_name']
+    last_name=request.POST['last_name']
+    password1 = request.POST['password1']
+    password2 = request.POST['password2']
+
+    if password1 != password2:
+        messages.error(request, "Passwords do not match.")
+        return redirect('/register')
+
+    user = User.objects.create_user(username, email, password1)
+    user.first_name = first_name
+    user.last_name = last_name
+    user.save()
+    return render(request, 'frontpage.html')
+
+def Login(request):
+    if request.method != "POST":
+        return render(request, "frontpage.html")
+    username = request.POST['username']
+    password = request.POST['password']
+
+    user = authenticate(username=username, password=password)
+
+    if user is not None:
+        login(request, user)
+        messages.success(request, "Successfully Logged In")
+        return redirect('/')
+    else:
+        messages.error(request, "Invalid Credentials")
+    return render(request ,'after_login.html')
+
+
+def Logout(request):
+    logout(request)
+    messages.success(request, "Successfully logged out")
+    return redirect('/login')
